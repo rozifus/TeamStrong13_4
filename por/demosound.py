a969ba70c0a1c9b7c67651687521d9c5b9e74f97
Jervis Whitley <jervis.whitley+git@gmail.com>
2013-04-20 00:13:36 +1000

    test for windows.

--- por/demosound.py
+++ por/demosound.py
@@ -1,3 +1,4 @@
+import sys
 import glob
 import os
 
@@ -5,12 +6,43 @@ import pyglet
 
 import settings
 
+if sys.platform == 'darwin':
+    from AppKit import NSSound
+    class Sound(object):
+        def __init__(self, path):
+            self.sound = NSSound.alloc()
+            self.sound.initWithContentsOfFile_byReference_(path, True)
+        def play(self):
+            self.sound.play()
+        def stop(self):
+            self.sound.stop()
+        @property
+        def playing(self):
+            return self.sound.isPlaying()
+elif sys.platform == 'win32':
+    from win32com.client import Dispatch
+    class Sound(object):
+        def __init__(self, path):
+            self.mp = Dispatch('WMPlayer.OCX')
+            self.sound = self.mp.newMedia(path)
+            self.mp.currentPlaylist.appendItem(self.sound)
+        def play(self):
+            self.mp.controls.play()
+        def stop(self):
+            self.mp.controls.stop()
+        @property
+        def playing(self):
+            return self.mp.playState == 9
 
 pyglet.resource.path = settings.RESOURCE_PATH
 pyglet.resource.reindex()
 
-sound = pyglet.resource.media('pink-turrican_gbc6.mp3')
-sound.play()
+if sys.platform in ('darwin', 'win32'):
+    sound = Sound('data/music/pink-turrican_gbc6.mp3')
+    sound.play()
+else:
+    sound = pyglet.resource.media('pink-turrican_gbc6.mp3')
+    sound.play()
 
 while 1:
     import time
