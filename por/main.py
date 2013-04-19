13bb362d75311eea2986f009ac7c234ae252a5f5
Karl Urdevics <karl.urdevics@gmail.com>
2013-04-19 22:58:53 +1000

    partially broken cutscenes... random slowdown HELPgit add -A

--- por/main.py
+++ por/main.py
@@ -30,35 +30,39 @@ class Game(object):
         self.window.push_handlers(self.current_scene)
 
     def stop_scene(self):
+        print "Main is stopping scene " + str(self.current_scene)
         self.current_scene.stop()
         self.window.pop_handlers()
+        print "on_draw popped, on draw exists? " + str(hasattr(self.window, "on_draw"))
         self.window.clear()
 
     def scene_finished(self, result, skip=None):
         print "Scene finished, result was " + str(result)
+        print "Scene was " + str(self.current_scene)
         
         self.stop_scene()
 
-        if result == "show_cutscene":
-            print "Showing cutscene"
-            self.current_scene = cutscene.Cutscene(self, title="cutscene!!", python="Hello there", ruby="Hello!", status="Fuck you")
-            self.start_scene()
-
-        elif result == "cutscene_finished":
-            self.current_scene = menu.MainMenu(self)
-            self.start_scene()
-
-        elif result == "play_game" or result == "level_finished":
+        if result == "play_game" or result == "level_finished":
             if skip:
-                index = skip - 1 
+                self.index = skip - 1 
             else:
-                index = self.loaded_level + 1
+                self.index = self.loaded_level + 1
 
-            Level = self.unfinished_levels[index]
-            self.current_scene = Level(self)
-
-            self.loaded_level = index
+            Level = self.unfinished_levels[self.index]
+            cutscene_params = settings.CUTSCENES[Level.name]
+            self.current_scene = cutscene.Cutscene(self,
+                    title=cutscene_params[0],
+                    python=cutscene_params[1],
+                    ruby=cutscene_params[2],
+                    status=cutscene_params[3])
             self.start_scene()
+        
+        elif result == "cutscene_finished":
+            Level = self.unfinished_levels[self.index]
+            self.current_scene = Level(self)
+            self.loaded_level = self.index
+            self.start_scene()
+
 
 def run():
     game = Game()
